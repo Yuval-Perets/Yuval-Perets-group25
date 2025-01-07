ab027f0ca1c1220e1194c3c741361cfc4b5c03b7
roeyrach <93585090+roeyrach@users.noreply.github.com>
2025-01-07 12:34:30 +0200

    continue

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById("submit-button").addEventListener('click', function() {
+    window.location.href = 'hompage.html';
+});
+
+document.getElementById("signin-button").addEventListener('click', function() {
+    window.location.href = 'hompage.html';
+});
+
